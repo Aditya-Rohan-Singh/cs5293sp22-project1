21e6ebfa468f736fbe8a328cdd2d7d42659feb0c
Aditya-Rohan-Singh <aditya.rohan.singh-1@ou.edu>
2022-04-03 04:55:14 +0000

    Added all functions and arguments. Added stats

--- project1/project1.py
+++ project1/project1.py
@@ -3,6 +3,9 @@ import spacy
 import re
 from dateparser.search import search_dates
 import warnings
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uReq
+from urllib.error import HTTPError
 
 warnings.filterwarnings(
     "ignore",
@@ -20,41 +23,95 @@ def read_inputfiles(input):
         lines=f.read().replace('\n','. ')
         sentences = list(map(str.strip, lines.split(". ")))
         sentences = list(filter(None,sentences))
-        #print(sentences)
         return(sentences)
     f.close()
 
-def redact_sentence(sentence):
+def redact_sentence(sentence,syn_list):
     
-    #Remove 10 digit phone numbers
-    sentence = re.sub(r'\d{10}','\u2588',sentence)
+    counter = 0
+    count_phone = 0
+    count_date = 0
+    count_gender = 0
+    count_name = 0
+    count_address = 0
+
+    #Redacting concepts
+    count_concept = 0
+    for word in syn_list:
+        #sentence = re.sub(word,'\u2588',sentence)
+        lenconcept = re.findall(word,sentence)
+        if (len(lenconcept) > 0):
+            counter = 1
+            count_concept = count_concept + len(lenconcept)
     
-    #Remove dates
-    matches = search_dates(sentence)
-    if matches is not None:
-        for x in matches:
-            sentence = re.sub(x[0],'\u2588',sentence)
-    #Remove gender related terms
-    gender_terms=['him','her','his','male','female','mother','father','aunt','uncle','niece','nephew','son','daughter','he','she','man','woman','boy','girl','husband','wife','actor','actress']
+    #If there are no concept words in the sentence.
+    if(counter == 0):
+    #Remove 10 digit phone number
+        # for fromat xxxxxxxxxx
+        sentence, count = re.subn(r'\d{10}','\u2588',sentence)
+        count_phone = count_phone + count
+        
+        #for fromat (xxx)-xxxxxxx
+        sentence, count = re.subn(r'[(]\d{3}[)]-\d{7}','\u2588',sentence)
+        count_phone = count_phone + count
+        
+        #for format (xxx)-xxx-xxxx
+        sentence,count = re.subn(r'[(]\d{3}[)]-\d{3}-\d{4}','\u2588',sentence)
+        count_phone = count_phone + count
+    
+        #Remove dates
+        matches = search_dates(sentence)
+        if matches is not None:
+            for x in matches:
+                sentence = re.sub(x[0],'\u2588',sentence)
+            count_date = len(matches)
 
-    #Removes names
-    doc = nlp(sentence)
-    redacted_sentence = []
-    for token in doc:
-        if token.ent_type_ == 'PERSON' or token.ent_type == 'GPE' or token.ent_type == 'DATE' or token.ent_type == 'ORG':
-            redacted_sentence.append('\u2588')
-            redacted_sentence.append(' ')
-        else:
-            redacted_sentence.append(token.text)
-            redacted_sentence.append(' ')
-    final_sentence = "".join(redacted_sentence)
-    return(final_sentence)
+        #Remove gender related terms
+        gender_terms=['him','her','is','male','female','mother','father','aunt','uncle','niece','nephew','son','daughter','he','she','man','woman','boy','girl','husband','wife','actor','actress']
+        for term in gender_terms:
+            lengender = re.findall(r"\b" + re.escape(term) + r"\b", sentence.lower())
+            if (len(lengender) > 0):
+                sentence,count = re.subn(r"\b" + re.escape(term) + r"\b",'\u2588',sentence.lower())
+                count_gender = count_gender + count
+        
+        #Remove address
+        sentence, count = re.subn(r'[0-9]{1-4} .+, .+ [A-Z]{2} [0-9]{5}',"\u2588",sentence)
+        count_address = count_address + count
 
-#if __name__ == '__main__':
-#    input_files=input_file_name('*.txt')
-#    for filename in input_files:
-#        list_sentences = read_inputfiles(filename)
-#        for single_sentence in list_sentences:
-#            redacted_sentence = redact_sentence(single_sentence)
-#            print(redacted_sentence)
-            
+        #Removes names
+        doc = nlp(sentence)
+        redacted_sentence = []
+        for token in doc:
+            if token.ent_type_ == 'PERSON' or token.ent_type == 'DATE' or token.ent_type == 'ORG':
+                redacted_sentence.append('\u2588')
+                redacted_sentence.append(' ')
+                count_name = count_name + 1
+            else:
+                redacted_sentence.append(token.text)
+                redacted_sentence.append(' ')
+        final_sentence = "".join(redacted_sentence)
+
+        return(final_sentence,count_concept,count_phone,count_date,count_gender,count_address,count_name)
+    else:
+        final_sentence = '\u2588'
+        return(final_sentence,count_concept,count_phone,count_date,count_gender,count_address,count_name)
+
+def find_syn(word):
+    stripped_string = word.strip()
+    fixed_string = stripped_string.replace(" ", "_")
+    my_url = f'https://thesaurus.plus/synonyms/{fixed_string}'
+
+    uClient = uReq(my_url)
+    page_html = uClient.read()
+    uClient.close()
+
+    page_soup = soup(page_html, "html.parser")
+    word_boxes = page_soup.find("ul", {"class": "list paper"})
+    results = word_boxes.find_all("div", "list_item")
+    syn_list = []
+    for result in results:
+        test = result.text.split('   ')
+        string = re.sub('^ ','',test[1])
+        syn_list.append(string)
+    return(syn_list)
+
--- project1/redactor.py
+++ project1/redactor.py
@@ -2,13 +2,25 @@
 import argparse
 import re
 import os.path
-from project1 import input_file_name, read_inputfiles, redact_sentence
+from project1 import input_file_name, read_inputfiles, redact_sentence, find_syn
 
-def main(input,output):
+def main(input,output,concepts,stats):
     print("Output Folder Location:",output,"\n\n")
+    
+    #Opening Stats file
+    std = open(stats,"w")
+    
+    #Creating Concept word list
+    syn_list = []
+    syn_list.extend(concepts)
+    for concept in concepts:
+        syn_list.extend(find_syn(concept))
+    print("Concept Words: ",syn_list,"\n\n")
+    
     #Loop to cycle through multiple input types
     for file_type in input:
         input_files=input_file_name(file_type)
+
         #If case if there are no files under the input type
         if len(input_files) == 0:
             print("No files found under input : ",file_type)
@@ -19,6 +31,12 @@ def main(input,output):
             #Loop to cycle through each text file
             for filename in input_files:
                 print("Filename: ",filename)
+                final_count_concept = 0
+                final_count_phone = 0
+                final_count_date = 0
+                final_count_gender = 0
+                final_count_name = 0
+                final_count_address = 0
                 if re.search('.',str(filename)):
                     new_filename = filename.split('.')[0] + '.redacted'
                 else:
@@ -30,22 +48,51 @@ def main(input,output):
                     file_location = output + "/" + new_filename
                     print(file_location)
                     write_file = open(file_location,"w")
+                    std.write(new_filename)
+                    std.write("\n-----------------\n")
                     for single_sentence in list_sentences:
-                        redacted_sentence = redact_sentence(single_sentence)
+                        redacted_sentence,count_concept,count_phone,count_date,count_gender,count_address,count_name = redact_sentence(single_sentence,syn_list)
                         write_file.write(redacted_sentence)
                         write_file.write('\n')
+                        final_count_concept = final_count_concept + count_concept
+                        final_count_phone = final_count_phone + count_phone
+                        final_count_date = final_count_date + count_date
+                        final_count_gender = final_count_gender + count_gender
+                        final_count_address = final_count_address + count_address
+                        final_count_name = final_count_name + count_name
                         #print(redacted_sentence)
                     write_file.write('\n')
                     write_file.close()
                     print("Redacted File Name:",new_filename,"\n\n")
+                    stringdata = "Total concept related words: " + str(final_count_concept) + "\n"
+                    std.write(stringdata)
+                    stringdata = "Total Phone Numbers: " + str(final_count_phone) + "\n"
+                    std.write(stringdata)
+                    stringdata = "Total Date: " + str(final_count_date) + "\n"
+                    std.write(stringdata)
+                    stringdata = "Total Gender: " + str(final_count_gender) + "\n"
+                    std.write(stringdata)
+                    stringdata = "Total address: " + str(final_count_address) + "\n"
+                    std.write(stringdata)
+                    stringdata = "Total Name: " + str(final_count_name) + "\n"
+                    std.write(stringdata)
+                    std.write("\n\n")
+    std.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Program to redact documents")
     parser.add_argument("--input", type=str, required=True, action='append', help="Type of Input files")
+    parser.add_argument("--names", action = 'store_true', required=True,  help="Redact Names")
+    parser.add_argument("--dates", action = 'store_true', required=True,  help="Redact dates")
+    parser.add_argument("--phones", action = 'store_true', required=True,  help="Redact phones")
+    parser.add_argument("--genders", action = 'store_true', required=True,  help="Redact genders")
+    parser.add_argument("--address", action = 'store_true', required=True,  help="Redact address")
+    parser.add_argument("--concept", type=str, required=True, action='append', help="Type of Concept")
     parser.add_argument("--output", type=str, required=True,  help="Location of output files")
+    parser.add_argument("--stats", type=str, required=True, help="File name for stats")
     args = parser.parse_args()
-    if args.input and args.output:
+    if args.input and args.output and args.stats and args.concept:
         if os.path.exists(args.output):
-            main(args.input,args.output)
+            main(args.input,args.output,args.concept,args.stats)
         else:
             print("Output path does not exists. Please provide correct path")
